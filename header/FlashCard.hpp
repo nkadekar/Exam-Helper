5a3f2a6b274909e07dbc87c288194d66701e433c
nkadekar <nkadekar7@gmail.com>
2020-11-18 12:57:03 -0800

    Fixes #14

--- header/FlashCard.hpp
+++ header/FlashCard.hpp
@@ -1,3 +1,42 @@
 #ifndef FLASHCARD_HPP
 #define FLASHCARD_HPP
+
+#include <string>
+
+using namespace std;
+
+class FlashCard {
+	private:
+		string term;
+		string definition;
+		bool used;
+
+	public:
+		FlashCard(string term, string definition) {
+			this->term = term;
+			this->definition = definition;
+			this->used = false;
+		}
+
+		string getTerm(){
+			return "";
+		}
+
+		string getDefinition(){
+			return definition;
+		}
+
+		bool getUsed(){
+			return used;
+		}
+
+		void setUsedToTrue(){
+			this->used = true;
+		}
+
+		void setUsedToFalse(){
+			this->used = false;
+		}
+};
+
 #endif
--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,3 +1,45 @@
+#include "gtest/gtest.h"
+
+#include "../header/FlashCard.hpp"
+
+TEST(FlashCardTest, ConstructorTest){
+	FlashCard* test = new FlashCard("Test", "This is a test");
+	
+	EXPECT_EQ(test->getTerm(), "Test");
+	EXPECT_EQ(test->getDefinition(), "This is a test");
+	EXPECT_EQ(test->getUsed(), false);
+}
+
+TEST(FlashCardTest, UsedSetterFunctions){
+	FlashCard* test = new FlashCard("Test", "This is a test");
+	
+	EXPECT_EQ(test->getUsed(), false);
+
+	test->setUsedToTrue();
+
+	EXPECT_EQ(test->getUsed(), true);
+
+	test->setUsedToTrue();
+
+	EXPECT_EQ(test->getUsed(), true);
+
+	test->setUsedToFalse();
+
+	EXPECT_EQ(test->getUsed(), false);
+
+	test->setUsedToFalse();
+
+	EXPECT_EQ(test->getUsed(), false);
+}
+
+TEST(FlashCardTest, EmptyStrings){
+	FlashCard* test = new FlashCard("", "");
+
+	EXPECT_EQ(test->getTerm(), "");
+	EXPECT_EQ(test->getDefinition(), "");
+	EXPECT_EQ(test->getUsed(), false);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
