56c0f7a282c8a4285d5559a1f09b17bbeeadc518
nkadekar <nkadekar@github.com>
2020-11-19 22:00:15 -0800

    Fixes #15

--- header/FlashCardList.hpp
+++ header/FlashCardList.hpp
@@ -3,6 +3,7 @@
 
 #include "FlashCard.hpp"
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -15,10 +16,28 @@ class FlashCardList {
 			return set.size();
 		}
 
-		FlashCard* at(i){
+		FlashCard* at(int i){
 			return set.at(i);
 		}
 
-		
+		void removeFlashCard(int i){
+			delete set.at(i);
+			set.erase(set.begin() + i);
+		}
 
+		void addFlashCard(string term, string definition){
+			set.push_back(new FlashCard(term, definition));
+		}
+
+		string displayFlashCards(){
+			stringstream inSS;
+			inSS << "Flashcards" << endl;
+			for (int i = 0; i < set.size(); i++){
+				inSS << i + 1 << ". " << set.at(i)->getTerm() << " : " << set.at(i)->getDefinition() << endl;
+				inSS << endl;
+			}
+			return inSS.str();
+		}
 };
+
+#endif
