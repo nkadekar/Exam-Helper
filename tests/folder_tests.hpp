d028cf3835a503d00068e324b21539529a64ea06
nkadekar <nkadekar7@gmail.com>
2020-12-06 22:32:43 -0800

    Updated folder_test

--- tests/folder_tests.hpp
+++ tests/folder_tests.hpp
@@ -38,43 +38,28 @@ TEST(FolderTest, renameFolder) {
     EXPECT_EQ(test->getName(), "CS111");
 }
 
-// TEST(FolderTest, addExam) {
-//     Folder *test = new Folder();
-//     test->addChildExam("CS100", "December 6, 2020");
-//     test->addChildExam("CS061", "December 18, 2020");
+TEST(FolderTest, addExam) {
+    Folder *test = new Folder();
+    test->addChildExam("CS100", "December 6, 2020");
+    test->addChildExam("CS061", "December 18, 2020");
 
-//     ASSERT_EQ(test->getSize(), 2);
-//     EXPECT_EQ(test->at(0)->getName(), "CS100");
-//     EXPECT_EQ(test->at(0)->getDate(), "December 6, 2020");    
-//     EXPECT_EQ(test->at(1)->getName(), "CS061");
-//     EXPECT_EQ(test->at(1)->getDate(), "December 18, 2020");
-// }
+    ASSERT_EQ(test->getSize(), 2);
+    EXPECT_EQ(test->at(0)->getName(), "CS100");
+    EXPECT_EQ(test->at(0)->getDate(), "December 6, 2020");    
+    EXPECT_EQ(test->at(1)->getName(), "CS061");
+    EXPECT_EQ(test->at(1)->getDate(), "December 18, 2020");
+}
 
-// TEST(FolderTest, removeExam) {
-//     Folder *test = new Folder();
-//     test->addChildExam("CS100", "December 6, 2020");
-//     test->addChildExam("CS061", "December 18, 2020");
-//     ASSERT_EQ(test->getSize(), 2);
+TEST(FolderTest, removeExam) {
+    Folder *test = new Folder();
+    test->addChildExam("CS100", "December 6, 2020");
+    test->addChildExam("CS061", "December 18, 2020");
+    ASSERT_EQ(test->getSize(), 2);
 
-//     test->removeChildExam(1);
-//     ASSERT_EQ(test->getSize(), 1);
-//     EXPECT_EQ(test->at(0)->getName(), "CS100");
-//     EXPECT_EQ(test->at(0)->getDate(), "December 6, 2020");
-// }
-
-// TEST(FolderTest, printTest) {
-//     Folder* test = new Folder();
-//     test->addChildFolder("CS100");
-//     test->addChildFolder("CS061");
-//     test->addChildFolder("CS111");
-
-//     testing::internal::CaptureStdout();
-//     test->print();
-//     std::string output = testing::internal::GetCapturedStdout();
-
-//     ASSERT_EQ(test->getSize(), 3);
-
-//     EXPECT_EQ(test->print(), "1 - CS100\n 2 - CS061\n 3 - CS111\n");
-// }
+    test->removeChildExam(1);
+    ASSERT_EQ(test->getSize(), 1);
+    EXPECT_EQ(test->at(0)->getName(), "CS100");
+    EXPECT_EQ(test->at(0)->getDate(), "December 6, 2020");
+}
 
 #endif //FOLDER_TESTS_HPP
